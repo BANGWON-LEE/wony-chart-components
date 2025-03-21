e06e59ae32bbeb694e467d874a16dd859bb44766
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-21 15:00:28 +0900

    edit : 차트 옵션 관련 코드 수정

--- renderer/components/chart/line/LineMain.jsx
+++ renderer/components/chart/line/LineMain.jsx
@@ -1,4 +1,4 @@
-import { Chart, Line } from 'react-chartjs-2'
+import { Chart } from 'react-chartjs-2'
 import { setLineData } from './lineData'
 
 export default function LineMain(props) {
@@ -8,18 +8,30 @@ export default function LineMain(props) {
     responsive: false,
     plugins: {
       legend: {
-        position: 'top',
+        position: 'bottom',
       },
       title: {
         display: true,
+        position: 'top',
         text: 'Chart.js Line Chart',
+      },
+    },
+    scales: {
+      x: {
+        border: {
+          dash: [7, 9],
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
       },
     },
   }
 
   return (
     <div>
-      <div>fef</div>
       <div>
         <Chart data={data} options={options} width={1900} height={300} />
       </div>
