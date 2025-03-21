da5eb08a5a5a076add3de7ba2e84ffbc4db7a93d
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-21 14:59:41 +0900

    edit : 불필요한 코드 제거

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react'
-import { formatLineData } from '../components/chart/line/lineData'
 import { dummyData } from '../data/dummy'
-import AskChart from '../components/AskChart'
+import AskChart from '../components/chart/AskChart'
 
 export default function HomePage() {
   const [data, setData] = useState(dummyData)
 
-  const resultFormatLineData = formatLineData(data)
-
-  console.log('result', resultFormatLineData)
-
   return (
     <React.Fragment>
-      <AskChart type="line" data={resultFormatLineData} />
+      <AskChart type="line" data={data} />
     </React.Fragment>
   )
 }
