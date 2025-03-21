d9fc08d29c5679429f3177b700754a20397913c8
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-21 14:59:29 +0900

    create & edit : color를 위한 파일 생성 및 코드 수정

--- renderer/components/chart/line/lineData.js
+++ renderer/components/chart/line/lineData.js
@@ -1,3 +1,5 @@
+import { colorArr } from '../style/styleElement'
+
 export function formatLineData(data) {
   const result = data.data.reduce((acc, obj) => {
     Object.keys(obj).forEach(key => {
@@ -18,12 +20,13 @@ export function setLineData(data) {
 
   return {
     labels,
-    datasets: notLabelTitleArr.map(line => ({
+    datasets: notLabelTitleArr.map((line, index) => ({
       type: 'line',
       label: line.toString(), //그래프 분류되는 항목
       data: data[line], //실제 그려지는 데이터(Y축 숫자)
-      borderColor: 'rgb(255, 99, 132)', //그래프 선 color
-      backgroundColor: 'rgba(255, 99, 132, 0.5)', //마우스 호버시 나타나는 분류네모 표시 bg
+      borderColor: colorArr[index].rgb, //그래프 선 color
+      backgroundColor: colorArr[index].rgb, //마우스 호버시 나타나는 분류네모 표시 bg
+      borderWidth: 5,
     })),
   }
 }
