2f8fe80cf9f35a81b21cd43d14c8f710cf42b6dd
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-26 13:54:57 +0900

    edit : line차트 관련 초기값 코드 작성

--- renderer/components/chart/line/lineData.js
+++ renderer/components/chart/line/lineData.js
@@ -12,7 +12,7 @@ export function formatLineData(data) {
   return result
 }
 
-export function setLineData(data) {
+export function setLineData(data, styleState) {
   const labels = data['time']
 
   const dataTitle = Object.keys(data)
@@ -24,9 +24,26 @@ export function setLineData(data) {
       type: 'line',
       label: line.toString(), //그래프 분류되는 항목
       data: data[line], //실제 그려지는 데이터(Y축 숫자)
-      borderColor: colorArr[index].rgb, //그래프 선 color
-      backgroundColor: colorArr[index].rgb, //마우스 호버시 나타나는 분류네모 표시 bg
-      borderWidth: 5,
+      borderColor: styleState[index].borderColor, //그래프 선 color
+      backgroundColor: styleState[index].backgroundColor, //마우스 호버시 나타나는 분류네모 표시 bg
+      borderWidth: styleState[index].borderWidth,
     })),
   }
+}
+
+export function setInitialLineStyle(lineData) {
+  const dataTitle = Object.keys(lineData)
+  const notLabelTitleArr = dataTitle.filter(el => el.toString() !== 'time')
+
+  const sizeLineData = Object.keys(notLabelTitleArr).length // time을 제외한 속성의 개수
+  const styleStateObjArr = Array.from({ length: sizeLineData }, (_, index) => ({
+    id: 'line' + index,
+    name: notLabelTitleArr[index],
+    borderColor: colorArr[index].rgb, //그래프 선 color
+    backgroundColor: colorArr[index].rgb, //마우스 호버시 나타나는 분류네모 표시 bg
+    borderWidth: 5,
+    type: 'line',
+  }))
+
+  return styleStateObjArr
 }
