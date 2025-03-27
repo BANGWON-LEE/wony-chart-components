08e507461dd003f485df5e944af2c5b7b9bd5b42
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-27 11:37:36 +0900

    create: style 적용을 위한 파일 추가

---
+++ renderer/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: './renderer/tailwind.config.js',
+    },
+    autoprefixer: {},
+  },
+}
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
