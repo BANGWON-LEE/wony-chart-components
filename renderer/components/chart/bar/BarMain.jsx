6ee8ca403a243bfe430bedb0ba9de8f3c5dfe316
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-27 17:57:55 +0900

    create : 새로운 타입 차트 추가

---
+++ renderer/components/chart/bar/BarMain.jsx
@@ -0,0 +1,39 @@
+import { Chart } from 'react-chartjs-2'
+
+export default function BarMain(props) {
+  const { data, width, height } = props
+
+  const options = {
+    responsive: false,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        position: 'top',
+        text: 'Chart.js Bar Chart',
+      },
+    },
+    scales: {
+      x: {
+        border: {
+          dash: [7, 9],
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  }
+
+  return (
+    <div>
+      <div>
+        <Chart data={data} options={options} width={width} height={height} />
+      </div>
+    </div>
+  )
+}
---
+++ renderer/components/chart/bar/barData.js
@@ -0,0 +1,53 @@
+import { colorArr } from '../style/styleElement'
+
+export function formatBarData(data) {
+  const result = data.data.reduce((acc, obj) => {
+    Object.keys(obj).forEach(key => {
+      acc[key] = acc[key] || []
+      acc[key].push(obj[key])
+    })
+    return acc
+  }, {})
+
+  return result
+}
+
+export function setBarData(data, styleState) {
+  const timePropertyName = 'time' // api를 통해서 받아오은 시간 값 프로퍼티 명을 입력하세요
+  const labels = data[timePropertyName]
+
+  const dataTitle = Object.keys(data)
+  const notLabelTitleArr = dataTitle.filter(
+    el => el.toString() !== timePropertyName
+  )
+
+  return {
+    labels,
+    datasets: notLabelTitleArr.map((line, index) => ({
+      type: 'bar',
+      label: line.toString(), //그래프 분류되는 항목
+      data: data[line], //실제 그려지는 데이터(Y축 숫자)
+      backgroundColor: styleState[index].backgroundColor, //마우스 호버시 나타나는 분류네모 표시 bg
+      barThickness: styleState[index].barThickness,
+    })),
+  }
+}
+
+export function setInitialBarStyle(lineData) {
+  const timePropertyName = 'time' // api를 통해서 받아오은 시간 값 프로퍼티 명을 입력하세요
+  const dataTitle = Object.keys(lineData)
+  const notLabelTitleArr = dataTitle.filter(
+    el => el.toString() !== timePropertyName
+  )
+
+  const sizeLineData = Object.keys(notLabelTitleArr).length // time을 제외한 속성의 개수
+  const styleStateObjArr = Array.from({ length: sizeLineData }, (_, index) => ({
+    id: 'bar' + index,
+    name: notLabelTitleArr[index],
+    backgroundColor: colorArr[index].rgb, //마우스 호버시 나타나는 분류네모 표시 bg
+    barThickness: 55,
+    type: 'bar',
+  }))
+
+  return styleStateObjArr
+}
