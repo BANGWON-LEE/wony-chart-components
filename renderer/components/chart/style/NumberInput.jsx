5ee1d684aa767ec938e838a39500eee0fef12418
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-27 16:58:08 +0900

    add : width 추가

--- renderer/components/chart/style/NumberInput.jsx
+++ renderer/components/chart/style/NumberInput.jsx
@@ -2,13 +2,14 @@ export default function NumberInput(props) {
   const { title, defaultValue, onChange, index } = props
 
   return (
-    <>
+    <div>
       <label>{title} : </label>
       <input
         type="number"
+        className="w-[70px]"
         defaultValue={defaultValue}
         onChange={event => onChange(event.currentTarget.value, index)}
       />
-    </>
+    </div>
   )
 }
