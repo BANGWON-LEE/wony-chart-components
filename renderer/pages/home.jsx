ec210d8af85dea13f65af50544d7b15733a7d171
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-27 11:38:13 +0900

    add : width와 height props 추가

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { dummyData } from '../data/dummy'
 import AskChart from '../components/chart/AskChart'
+// import '../global.css'
 
-export default function HomePage() {
+export default function App() {
   const [data, setData] = useState(dummyData)
 
   return (
     <React.Fragment>
-      <AskChart type="line" data={data} />
-      <AskChart type="mixed" data={data} />
+      <AskChart type="line" data={data} width={1300} height={480} />
+      <AskChart type="mixed" data={data} width={1300} height={480} />
     </React.Fragment>
   )
 }
