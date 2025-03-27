d3fb3b923cc8ae06c4cb11c4ff546056a15aa04c
BANGWON LEE <qkddnjs31@gmail.com>
2025-03-27 11:38:38 +0900

    add : 스타일 커스텀 컴포넌트 모달 팝업 구현

--- renderer/components/chart/StyleCustomFilter.jsx
+++ renderer/components/chart/StyleCustomFilter.jsx
@@ -2,7 +2,7 @@ import NumberInput from './style/NumberInput'
 import { colorArr } from './style/styleElement'
 
 export default function StyleCustomFilter(props) {
-  const { styleState, setStyleState, type } = props
+  const { styleState, setStyleState, type, closeModalBtn } = props
 
   function updateBorderWidth(event, index) {
     setStyleState(prev => {
@@ -49,63 +49,68 @@ export default function StyleCustomFilter(props) {
   const chartType = ['line', 'bar']
 
   return (
-    <div>
-      {styleState.map((el, index) => (
-        <div key={el.id}>
-          <p>{el.name}</p>
-          {el.type === 'line' && (
-            <NumberInput
-              title={'lineBorderWidth'}
-              defaultValue={el.borderWidth}
-              onChange={updateBorderWidth}
-              index={index}
-            />
-          )}
-          {el.type === 'bar' && (
-            <NumberInput
-              title={'barWidth'}
-              defaultValue={el.barThickness}
-              onChange={updateBarThicknessColor}
-              index={index}
-            />
-          )}
-          <label>color :</label>
-          <select
-            onChange={event =>
-              updateBorderColor(event.currentTarget.value, index)
-            }
-          >
-            <option>색상을 선택하세요</option>
-            {colorArr.map(el => {
-              return (
-                <option key={el.name} value={el.rgb}>
-                  {el.name}
-                </option>
-              )
-            })}
-          </select>
-          {type === 'mixed' && (
-            <>
-              <label>chart type :</label>
+    <div className="fixed inset-0 flex items-center justify-center bg-slate-500 bg-opacity-20">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-[70vw]">
+        <button onClick={() => closeModalBtn()}>x</button>
+        {styleState.map((el, index) => (
+          <div key={el.id} className="flex justify-between">
+            <p>{el.name}</p>
+            {el.type === 'line' && (
+              <NumberInput
+                title={'lineBorderWidth'}
+                defaultValue={el.borderWidth}
+                onChange={updateBorderWidth}
+                index={index}
+              />
+            )}
+            {el.type === 'bar' && (
+              <NumberInput
+                title={'barWidth'}
+                defaultValue={el.barThickness}
+                onChange={updateBarThicknessColor}
+                index={index}
+              />
+            )}
+            <div>
+              <label className="mr-[15px]">color :</label>
               <select
                 onChange={event =>
-                  changeChartTypeInMixed(event.currentTarget.value, index)
+                  updateBorderColor(event.currentTarget.value, index)
                 }
-                defaultValue={styleState[index].type}
               >
-                <option disabled>{styleState[index].type}</option>
-                {chartType.map(el => {
+                <option>색상을 선택하세요</option>
+                {colorArr.map(el => {
                   return (
-                    <option key={'chartTypeArr' + el} value={el}>
-                      {el}
+                    <option key={el.name} value={el.rgb}>
+                      {el.name}
                     </option>
                   )
                 })}
               </select>
-            </>
-          )}
-        </div>
-      ))}
+            </div>
+            {type === 'mixed' && (
+              <div>
+                <label className="mr-[15px]">chart type :</label>
+                <select
+                  onChange={event =>
+                    changeChartTypeInMixed(event.currentTarget.value, index)
+                  }
+                  defaultValue={styleState[index].type}
+                >
+                  <option disabled>{styleState[index].type}</option>
+                  {chartType.map(el => {
+                    return (
+                      <option key={'chartTypeArr' + el} value={el}>
+                        {el}
+                      </option>
+                    )
+                  })}
+                </select>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
