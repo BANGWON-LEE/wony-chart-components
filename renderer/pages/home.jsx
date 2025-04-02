b5eb2251892ebc2b90146fcba25ec7d9cabcb3a8
BANGWON LEE <qkddnjs31@gmail.com>
2025-04-02 13:45:42 +0900

    add : 차트 고유 명칭 추가 및 formatData 함수 사용

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { dummyData } from '../data/dummy'
 import AskChart from '../components/chart/AskChart'
+import { formatData } from '../components/chart/common/common'
 // import '../global.css'
 
 export default function App() {
-  const [data, setData] = useState(dummyData)
+  const [data, setData] = useState(formatData(dummyData))
 
   return (
     <React.Fragment>
@@ -13,11 +14,29 @@ export default function App() {
         data={data}
         width={1300}
         height={480}
-        chartName={'test1'}
+        uniqueChartName={'test1'}
       />
-      {/* <AskChart type="mixed" data={data} width={1300} height={480} />
-      <AskChart type="bar" data={data} width={1300} height={480} />
-      <AskChart type="stackBar" data={data} width={1300} height={480} /> */}
+      <AskChart
+        type="mixed"
+        data={data}
+        width={1300}
+        height={480}
+        uniqueChartName={'test2'}
+      />
+      <AskChart
+        type="bar"
+        data={data}
+        width={1300}
+        height={480}
+        uniqueChartName={'test3'}
+      />
+      <AskChart
+        type="stackBar"
+        data={data}
+        width={1300}
+        height={480}
+        uniqueChartName={'test4'}
+      />
     </React.Fragment>
   )
 }
