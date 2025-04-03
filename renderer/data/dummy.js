75b162594b61b895c2d80372894f88280ba03ea4
BANGWON LEE <qkddnjs31@gmail.com>
2025-04-03 13:36:21 +0900

    edit : 더미데이터 추가

--- renderer/data/dummy.js
+++ renderer/data/dummy.js
@@ -1,3 +1,25 @@
+export function generateDataArray(length = 30) {
+  const dataArray = []
+  const getRandomValue = (min, max) =>
+    (Math.random() * (max - min) + min).toFixed(1)
+
+  for (let i = 0; i < length; i++) {
+    const time = new Date()
+    time.setMinutes(time.getMinutes() - i * 10) // 10분 간격 데이터
+
+    dataArray.push({
+      time: time.toISOString().slice(0, 16).replace('T', ' '), // 시간 형식: 'YYYY-MM-DD HH:MM'
+      temperature: parseFloat(getRandomValue(20, 30)), // 온도: 20 ~ 30도
+      humidity: parseFloat(getRandomValue(40, 60)), // 습도: 40 ~ 60%
+      co2: parseFloat(getRandomValue(30, 50)), // CO2: 30 ~ 50ppm
+    })
+  }
+
+  return dataArray
+}
+
+console.log(generateDataArray())
+
 export const dummyData = {
   data: [
     {
