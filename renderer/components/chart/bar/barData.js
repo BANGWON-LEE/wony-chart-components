127c2548f091b528da217318e6dd40ac928906dc
BANGWON LEE <qkddnjs31@gmail.com>
2025-04-03 10:46:48 +0900

    edit : 불필요한 코드 제거

--- renderer/components/chart/bar/barData.js
+++ renderer/components/chart/bar/barData.js
@@ -1,18 +1,6 @@
 import { getStorage } from '../common/common'
 import { colorArr } from '../style/styleElement'
 
-export function formatBarData(data) {
-  const result = data.data.reduce((acc, obj) => {
-    Object.keys(obj).forEach(key => {
-      acc[key] = acc[key] || []
-      acc[key].push(obj[key])
-    })
-    return acc
-  }, {})
-
-  return result
-}
-
 export function setBarData(data, styleState) {
   const timePropertyName = 'time' // api를 통해서 받아오은 시간 값 프로퍼티 명을 입력하세요
   const labels = data[timePropertyName]
@@ -22,6 +10,8 @@ export function setBarData(data, styleState) {
     el => el.toString() !== timePropertyName
   )
 
+  // console.log('styleState', styleState)
+
   return {
     labels,
     datasets: notLabelTitleArr.map((line, index) => ({
@@ -29,6 +19,7 @@ export function setBarData(data, styleState) {
       label: line.toString(), //그래프 분류되는 항목
       data: data[line], //실제 그려지는 데이터(Y축 숫자)
       backgroundColor: styleState?.[index].backgroundColor, //마우스 호버시 나타나는 분류네모 표시 bg
+      borderRadius: styleState?.[index].borderRadius,
       barThickness: styleState?.[index].barThickness,
     })),
   }
@@ -49,6 +40,7 @@ export function setInitialBarStyle(lineData, uniqueChartName) {
     name: notLabelTitleArr[index],
     backgroundColor: styleArr?.[index].backgroundColor || colorArr[index].rgb, //마우스 호버시 나타나는 분류네모 표시 bg
     barThickness: styleArr?.[index].barThickness || 55,
+    borderRadius: 10,
     type: 'bar',
   }))
 
