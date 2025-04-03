0605b49b099341fd19ddc0e609b67b752a962b8f
BANGWON LEE <qkddnjs31@gmail.com>
2025-04-03 13:35:33 +0900

    edit : 버튼 태그 위치 이동

--- renderer/components/chart/AskChart.jsx
+++ renderer/components/chart/AskChart.jsx
@@ -99,8 +99,8 @@ export default function AskChart(props) {
 
   return (
     <>
-      {chartUi()}
       <button onClick={() => handleOpenStyleFilterModal()}>open</button>
+      {chartUi()}
       {openCustomFilterModalState && (
         <StyleCustomFilter
           closeModalBtn={handleOpenStyleFilterModal}
